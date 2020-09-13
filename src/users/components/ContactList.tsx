4f590af63d240acd7f3612b640ae0fa438e747fb
rachel-os <rachel.otete@gmail.com>
2020-09-13 18:32:44 +0200

    test creating new component

---
+++ src/users/components/ContactList.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+//Le composant est d'abord créé sans user.
+
+export default class ContactList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Liste de contact</p>
+      </div>
+    )
+  }
+}
