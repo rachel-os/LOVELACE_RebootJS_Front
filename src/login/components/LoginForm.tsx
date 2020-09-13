60de6d77a4e42ab1e0f6a9041f480033f37f867e
rachel-os <rachel.otete@gmail.com>
2020-09-13 22:16:36 +0200

    add error handling on Login Page

--- src/login/components/LoginForm.tsx
+++ src/login/components/LoginForm.tsx
@@ -1,13 +1,15 @@
 import { Box, Button, Container, Grid,TextField  } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 import React, { Component } from 'react';
 import history from '../../history';
 import { login } from '../../api/methods';
 import { IFormField, defaultFormField } from '../../utils/types';
-
+import { validateRequiredField } from '../../utils/validateRequiredField';
 
 interface LoginFormState {
   email: IFormField,
   password: IFormField,
+  status: 'ready' | 'success' | 'error'
 }
 
 // S'il existe déjà des valeurs initiales sur mon profil, alors props pour les faire apparaître au premier chargement de la page. 
@@ -18,7 +20,8 @@ export default class LoginForm extends Component<{}, LoginFormState> {
     super(props)
     this.state = {
       email: defaultFormField(),
-      password: defaultFormField()
+      password: defaultFormField(),
+      status: 'ready'
     }
     this.submit = this.submit.bind(this)
     // équivaut à définir submit en arrow function : submit = () => {}
@@ -26,35 +29,53 @@ export default class LoginForm extends Component<{}, LoginFormState> {
 
   submit() {
     login(this.state.email.value, this.state.password.value)
-      .then((_profile) => history.push('/profiles'));
-  }
+      .then((_profile) => {
+        history.push('/')
+        // this.setState({status: 'success'})
+      })
+      .catch(_error => { this.setState({status: 'error'})});
+    }
 
   render() {
+    const { email, password, status } = this.state;
     return (
       <Container maxWidth='xs'>
           <form onSubmit={(event) => { event.preventDefault(); this.submit() }}>
             {/* event.preventDefault : j'empêche son comportement par défaut et je lui demande autre chose, en l'occurence de submit */}
+            
+            {/* en fonction du status, je définis un message à renvoyer à l'utilisateur. */}
+            {status !== 'ready' ?
+              <Alert severity={status}>
+                {status === 'success' ? 'User is logged in.' : 'User does not exist.'}
+              </Alert> : null }
             <Box style={{ margin: '2rem 0'}}>
               <TextField
                 label="Email"
-                value={this.state.email.value}
+                value={email.value}
                 required={true}  
                 //je récupère tout ce qui existe dans le state et ensuite l'email 
                 onChange={(event) => this.setState({
-                  ...this.state,
-                  email: {value: event.target.value, isValid: true}
+                  email: {value: event.target.value, isValid: validateRequiredField(event.target.value)}
                     // event = changements sur le field - target = le field - value = le contenu du field
                 })}
+                fullWidth={true}
+                style={{margin: '1rem 0'}}
+                variant="outlined"
+                {...( email.isValid ? {} : { error: true, helperText: 'This field is required.' })}
               />
             <TextField
               type="password"
               label="Password"
               required={true}
-              value={this.state.password.value}
+              value={password.value}
               onChange={(event) => this.setState({
                 ...this.state,
-                password: {value: event.target.value, isValid: true}
+                password: {value: event.target.value, isValid: validateRequiredField(event.target.value)}
               })}
+              fullWidth={true}
+              variant="outlined"
+              {...( password.isValid ? {} : { error: true, helperText: 'This field is required.' })}
+
             />
           </Box>
           <Box style={{margin: '1rem 0'}}>
